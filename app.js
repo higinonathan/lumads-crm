82b47a92de8263b8cd4db38b203cc93375d1fd0f
higinonathan <criptohigino@gmail.com>
2026-09-04 18:56:36 -0400

    Aplica layout aprovado da Central de Configurações

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ import './lumads-approval-actions.css';
 import './lumads-clients-page.css';
 import './client-detail-approved.css';
 import './lumads-history-page.css';
+import './lumads-settings-page.css';
 import './app-core.js';
